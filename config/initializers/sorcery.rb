b660eb9e20ace67d99f5ca7c45e69f4db8d65d97
estsn122 <y.tani9411@gmail.com>
2020-04-12 10:58:18 +0900

    Add:twitterログイン機能実装

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -5,12 +5,12 @@ Rails.application.config.sorcery.configure do |config|
 
   config.twitter.key = Rails.application.credentials.dig(:twitter, :api_key)
   config.twitter.secret = Rails.application.credentials.dig(:twitter, :api_secret_key)
-  config.twitter.callback_url = 'http://0.0.0.0:3000/oauth/callback?provider=twitter'
-  config.twitter.user_info_mapping = {:email => "screen_name"}
+  config.twitter.callback_url = 'http://localhost:3000/oauth/callback?provider=twitter'
+  config.twitter.user_info_mapping = {:email => 'screen_name'}
 
   config.user_config do |user|
     user.stretches = 1 if Rails.env.test?
     user.authentications_class = Authentication
   end
-  config.user_class = "User"
+  config.user_class = 'User'
 end
