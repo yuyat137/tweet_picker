38841c071fb062b3a05f14e1ca604f04387c3bd6
estsn122 <y.tani9411@gmail.com>
2020-04-12 18:20:23 +0900

    Add:暗号化してアクセストークンを保存

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,5 @@
 class Authentication < ApplicationRecord
   belongs_to :user
+  attr_encrypted :access_token, key: Rails.application.credentials.encryption_key
+  attr_encrypted :access_token_secret, key: Rails.application.credentials.encryption_key
 end
