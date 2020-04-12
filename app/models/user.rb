e6e4a0dfa5288f386b5cc6833b2e6548c463ff8e
estsn122 <y.tani9411@gmail.com>
2020-04-12 22:17:08 +0900

    Add:そのユーザーのtwitter_rest_clientを持ってくるインスタンスメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
+
+  def twitter_rest_client
+    @twitter_rest_client ||= Twitter::REST::Client.new do |config|
+      authentication = authentications.find_by(provider: 'twitter')
+      config.consumer_key        = Rails.application.credentials.dig(:twitter, :api_key)
+      config.consumer_secret     = Rails.application.credentials.dig(:twitter, :api_secret_key)
+      config.access_token        = authentication.access_token
+      config.access_token_secret = authentication.access_token_secret
+    end
+  end
 end
