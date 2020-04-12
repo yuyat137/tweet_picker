0da3854b2299464a1c8d7655afbf1f28d23a974e
estsn122 <y.tani9411@gmail.com>
2020-04-12 13:55:37 +0900

    Fix:不要なファイルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # NOTE: 恐らく下記2行不要
-  # get 'oauths/oauth'
-  # get 'oauths/callback'
   root 'users#new'
   get 'oauth/callback' => 'oauths#callback'
   get 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
