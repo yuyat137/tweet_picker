7ba0cccb9a486f390f3bb3877ef0ac7127b75acd
estsn122 <y.tani9411@gmail.com>
2020-04-18 20:33:54 +0900

    [Add]適切にフラッシュメッセージが表示されるよう実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,8 +1,10 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: :new
+
   def new; end
 
   def destroy
     logout
-    redirect_to root_path
+    redirect_to login_path, success: 'ログアウトしました'
   end
 end
