159e7ac03b003427135ad0e5781f6386da4dddd2
estsn122 <y.tani9411@gmail.com>
2020-04-19 12:36:10 +0900

    Add:profile_image_urlを保存

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -9,15 +9,16 @@ class OauthsController < ApplicationController
     provider = params[:provider]
     @user = login_from(provider)
     if @user
+      @user.update(profile_image_url: @user.twitter.user.profile_image_url.to_s)
       redirect_to twitter_lists_path, success: 'ログインしました'
     else
       begin
         @user = create_from(provider)
         # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
 
-        # アクセストークンの保存
         # TODO: ここのコードがもっと綺麗にならないか調査
         Authentication.find_by(uid: @access_token.params[:user_id]).update!(access_token: @access_token.token, access_token_secret: @access_token.secret)
+        @user.update(profile_image_url: @user.twitter.user.profile_image_url.to_s)
 
         reset_session
         auto_login(@user)
