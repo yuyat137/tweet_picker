5a5caf81592de078dcec1e2aecac8603519da2d7
estsn122 <y.tani9411@gmail.com>
2020-04-22 20:11:13 +0900

    [Add]リスト名を表示するよう実装

--- app/controllers/twitter_lists_controller.rb
+++ app/controllers/twitter_lists_controller.rb
@@ -7,6 +7,7 @@ class TwitterListsController < ApplicationController
     list = current_user.twitter_lists.find_by(access_id: params[:access_id])
     if list
       @tweets = current_user.twitter.list_timeline(list.list_id, count: 200).max_by(50, &:favorite_count)
+      @list_name = list.list_name
     else
       redirect_to twitter_lists_path, danger: 'リストを更新してください'
     end
