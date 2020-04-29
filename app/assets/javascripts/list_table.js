d33b13c86a6c890536ac3bfa127ec52abe0f6679
estsn122 <y.tani9411@gmail.com>
2020-04-29 10:43:53 +0900

    [Fix]ツイートが読み込めない場合に対処、その他簡単な微調整

--- app/assets/javascripts/list_table.js
+++ app/assets/javascripts/list_table.js
@@ -1,6 +1,6 @@
 $(function() {
     $('tr[data-href]').addClass('clickable');
-    $(document).on('click', function (e) {
+    $('#table_style').on('click', function (e) {
         window.location = $(e.target).closest('tr').data('href');
     });
 });
