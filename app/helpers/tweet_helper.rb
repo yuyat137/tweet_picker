bf6c930c8bf87f4779dabbfb243e1f20d8867207
estsn122 <y.tani9411@gmail.com>
2020-07-05 15:41:25 +0900

    [Add]リンクを正常に表示できるよう実装

--- app/helpers/tweet_helper.rb
+++ app/helpers/tweet_helper.rb
@@ -1,13 +1,23 @@
 module TweetHelper 
   def body(tweet)
     if tweet.media?
-      text = tweet.attrs[:full_text].sub(/ https:\/\/t\.co\/[0-9a-zA-Z]{10}$/, '')
+      text = tweet.attrs[:full_text].sub(/https:\/\/t\.co\/[0-9a-zA-Z]{10}\Z/, '')
     else
       text = tweet.attrs[:full_text]
     end
 
+    # t.co URLを通常のURLに戻す
     tweet.attrs[:entities][:urls].each do |urls|
       text.sub!(urls[:url], urls[:expanded_url])
+    end
+    # URLをリンクに変換する
+    url_texts = ['http://', 'https://']
+    url_texts.each do |url_text|
+      mark = text.match(url_text)
+      next unless mark
+      url = url_text + mark.post_match.split.first
+      link = "<a href='" + url + "'>" + url + "</a>"
+      text.gsub!(url, link)
     end
 
     # リプライの場合、もしくは(@)ユーザー名がツイート内にある場合
@@ -16,18 +26,23 @@ module TweetHelper
     end
 
     # ハッシュタグがある場合
-    # リンクにする
-
+    # TODO: 現状バグ有り。例：#ガルパと#ガルパ超電磁砲Tコラボ開催中がハッシュタグの場合
+    if tweet.attrs[:entities][:hashtags]
+      tweet.attrs[:entities][:hashtags].each do |hashtag|
+        link = "<a href='https://twitter.com/hashtag/" + hashtag[:text]+ "?src=hashtag_click'>#" + hashtag[:text] + "</a>"
+        text.gsub!('#' + hashtag[:text], link)
+      end
+    end
     #引用ツイートがある場合
     #引用したツイートを小さく表示する
+    # simple_format(text_url_to_link(h(text)))
 
-    simple_format(text_url_to_link(h(text)))
+    text
   end
 
   def media(tweet)
     if tweet.media.first.class == Twitter::Media::Photo
       # 画像表示
-      # TODO: width100%ではなく、heightを基準に表示する
       # TODO: 複数画像を添付してる場合にも対応する
       image_tag('https://pbs.twimg.com' + tweet.media.first.media_url.path, class: 'media')
     elsif tweet.media.first.class == Twitter::Media::Video
@@ -46,7 +61,7 @@ module TweetHelper
 
   def youtube_thumbnail(tweet)
     tweet_text = tweet.attrs[:full_text]
-    youtube_text = ['https://youtu.be', 'https://www.youtube.com/watch?v=']
+    youtube_text = ['youtu.be', 'youtube.com/watch?v=']
     return unless youtube_text.any? { |t| tweet_text.include?(t) }
     
     mark =
@@ -56,6 +71,7 @@ module TweetHelper
         %r{youtu.be/}.match(tweet_text)
       end
     
+    return unless mark
 
     content_tag 'iframe', nil, src: ('https://www.youtube.com/embed/' + mark.post_match.split.first), \
       frameborder: 0, gesture: 'media', allow: 'encrypted-media', allowfullscreen: true, class: 'embed_youtube'
