1692941b5069680fb408555913e1f3cc6876995d
estsn122 <y.tani9411@gmail.com>
2020-07-31 21:56:01 +0900

    [Add]jsファイルを調整

--- app/assets/javascripts/smartphone-twitter-icon.js
+++ app/assets/javascripts/smartphone-twitter-icon.js
@@ -1,33 +1,16 @@
 $(function(){
-  var $dropdown = $('.js-dropdown');
-  var DURATION = 200; //アニメーションの速さ
-
-  //以下2つの関数内で使用するメソッド
-  function fadeOutMenu(){
-    $dropdown.removeClass('is-active')
-      .next('.js-dropdown-menu')
-      .stop()
-      .slideUp(DURATION);
-  }
-
-  //表示を切り替える
   function toggleMenu(){
-    var $self = $(this); //thisにはクリックした時の要素が入る
-    //要素が.is-activeを持っていない場合
-    if(!$self.hasClass('is-active')){
-      fadeOutMenu();
-    }
-    //クリックした要素を表示させる
-    $self.toggleClass('is-active')
+    $(this).toggleClass('is-active')
       .next('.js-dropdown-menu')
-      .stop().slideToggle(DURATION);
-  }
-  $dropdown.on('click', toggleMenu);
- 
-  //別の場所をクリックすると閉じる処理
-  $(document).on('click touchend', function(event) {
-    if (!$(event.target).closest('body').length) {
-      fadeOutMenu();
+      .slideToggle(0);
+    if($(this).hasClass('is-active')){
+      $('#drop_down_arrow').css('color', 'white');
+      $('.smartphone-menu').css('z-index', '9999');
+    } else {
+      $('#drop_down_arrow').css('color', 'black');
+      $('.smartphone-menu').css('z-index', '9999');
     }
-  });
+  }
+
+  $('.js-dropdown').on('click', toggleMenu);
 });
