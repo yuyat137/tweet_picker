2fec31bc568219cee3edbff8c3873367f54f0903
estsn122 <y.tani9411@gmail.com>
2020-08-17 15:00:13 +0900

    [Add]herokuでutf8mb4を使うため、migrationファイルを更新

--- db/migrate/20200412012506_sorcery_core.rb
+++ db/migrate/20200412012506_sorcery_core.rb
@@ -1,6 +1,6 @@
 class SorceryCore < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :users, options: 'DEFAULT CHARSET=utf8mb4' do |t|
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
