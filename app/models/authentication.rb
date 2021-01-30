7475b45337b4d16403087441e279103f4867f0ea
estsn122 <y.tani9411@gmail.com>
2021-01-30 21:23:19 +0900

    [Add]ゲストユーザーを作成

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -2,4 +2,22 @@ class Authentication < ApplicationRecord
   belongs_to :user
   attr_encrypted :access_token, key: Rails.application.credentials.encryption_key
   attr_encrypted :access_token_secret, key: Rails.application.credentials.encryption_key
+
+  def self.twitter_access_token(user)
+    if user.email == 'guest'
+      Rails.application.credentials.dig(:guest, :access_token)
+    else
+      authentication = user.authentications.find_by(provider: 'twitter')
+      authentication.access_token
+    end
+  end
+
+  def self.twitter_access_token_secret(user)
+    if user.email == 'guest'
+      Rails.application.credentials.dig(:guest, :access_token_secret)
+    else
+      authentication = user.authentications.find_by(provider: 'twitter')
+      authentication.access_token_secret
+    end
+  end
 end
