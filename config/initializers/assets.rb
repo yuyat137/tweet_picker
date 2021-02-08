ecd66f38eae43f074ab47b2ced8eff02152b35db
estsn122 <y.tani9411@gmail.com>
2021-02-08 10:44:45 +0900

    [Fix]rubocopの指摘を修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( masonry.js )
+Rails.application.config.assets.precompile += %w(masonry.js)
 
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in the app/assets
